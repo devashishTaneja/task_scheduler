336bc37acedb1c8d490d604085c6a190a93407d8
Devashish Taneja <dtaneja@enphaseenergy.com>
2021-08-04 16:42:32 +0530

    Added uniqueness to index

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2021_08_04_104802) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "tasks", "users"
